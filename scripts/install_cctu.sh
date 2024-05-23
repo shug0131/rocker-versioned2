acc423db602f4acbc9bab65f4dceac07b38bfd48
shug0131 <sjb277@medschl.cam.ac.uk>
2024-05-23 15:03:47 +0100

    ensure that vignettes get built for cctu package

--- scripts/install_cctu.sh
+++ scripts/install_cctu.sh
@@ -21,7 +21,9 @@ apt_install \
 
 
 install2.r --error  --skipmissing --deps TRUE --skipinstalled -n "$NCPUS"  xslt
-R -q -e "remotes::install_github(repo=\"shug0131/cctu\", ref=\"$CCTU_VERSION\" )"
+
+R -q -e "remotes::install_github(repo=\"cam-ctu/cctu\", ref=\"$CCTU_VERSION\", INSTALL_opts=\"--install-tests\" , build_vignettes=TRUE)"
+
 
 # The GSL package requires this, and MICE needs gsl. I think Ubuntu:latest doesn't have it
 # so the devel version fails to build
